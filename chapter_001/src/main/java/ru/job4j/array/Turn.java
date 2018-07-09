473cd0606aeea83281dbd8cf55ec180321310c41
ilyaosipov3425 <bullet3425@yandex.ru>
2018-07-09 22:11:58 +0300

    6.2. "Перевернуть массив.[#4441]"

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+/**
+ * Программа переворачивающая массив
+ * @author Osipov Ilya (mailto:bullet3425@yandex.ru)
+ * @version $Id$
+ * @since 08.07.2018
+ */
+public class Turn {
+    public int[] turn(int[] array) {
+        for(int i = 0; i < array.length / 2; i++) {
+            int input = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = input;
+        }
+        return array;
+    }
+}
