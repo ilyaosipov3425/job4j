baa89f0f6b26a44d587ef30f3045ed5aa7b38307
Ilya Osipov <bullet3425@yandex.ru>
2019-02-15 18:11:11 +0300

    Конвертация листа массивов в один лист Integer.[#10037]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,5 +1,6 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,5 +30,15 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] i : list) {
+            for (int j : i) {
+                result.add(j);
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -2,7 +2,7 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -42,6 +42,19 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {0, 0, 0}
         };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenConvertListArraysToListInteger() {
+        ConvertList2Array convertList = new ConvertList2Array();
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        List<Integer> result = convertList.convert(list);
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6
+        );
         assertThat(result, is(expect));
     }
 }
